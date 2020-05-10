df32ea951636bf0d96887ae7ed500a171561def6
apastors <a.pastor.sanchez@gmail.com>
2020-05-10 17:02:17 +0100

    week 1 with some solved exercises

---
+++ week1/src/test/scala/scalashop/BlurSuite.scala
@@ -0,0 +1,56 @@
+package scalashop
+
+import VerticalBoxBlur._
+
+import java.util.concurrent._
+import scala.collection._
+import org.junit._
+import org.junit.Assert.assertEquals
+
+class BlurSuite {
+  
+  @Test def `test clamp`: Unit = {
+    assertEquals(clamp(5, 0, 10), 5)
+    assertEquals(clamp(5, 0, 2), 2)
+    assertEquals(clamp(-2, 0, 10), 0)
+  }
+
+  @Test def `test Filter`: Unit = {
+    val i = new Img(width=3, height=3, data=Array(
+        4, 4, 4,
+        4, 4, 4, 
+        1, 1, 1
+      )
+    )
+
+    assertEquals(4, boxBlurKernel(i, 1, 0, 1))
+    assertEquals(3, boxBlurKernel(i, 0, 1, 1))
+    assertEquals(3, boxBlurKernel(i, 1, 1, 1))
+  }
+
+  @Test def `test Blur`: Unit = {
+    val src = new Img(width=3, height=3, data=Array(
+        2, 0, 0,
+        2, 0, 0, 
+        2, 0, 0
+      )
+    )
+    val dst = new Img(width=3, height=3, data=Array(
+        0, 0, 0,
+        0, 0, 0, 
+        0, 0, 0
+      )
+    )
+    val expected = new Img(width=3, height=3, data=Array(
+        1, 0, 0,
+        1, 0, 0, 
+        1, 0, 0
+      )
+    )
+    blur(src, dst, from=0, end=1, radius=1)
+    for (x <- 0 until dst.width; y <- 0 until dst.height)
+      assertEquals(expected(x, y), dst(x, y))
+  }
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+}
