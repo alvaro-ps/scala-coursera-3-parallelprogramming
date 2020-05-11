ff3c2c454e3b89561fc4623e7f252cc8f3535ded
apastors <a.pastor.sanchez@gmail.com>
2020-05-11 16:46:07 +0100

    balance strings

--- week2/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ week2/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -13,7 +13,7 @@ object ParallelParenthesesBalancingRunner {
     Key.exec.minWarmupRuns -> 40,
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 120,
-    Key.verbose -> true
+    Key.verbose -> false
   ) withWarmer(new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
@@ -40,22 +40,46 @@ object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterfac
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def cum_balance(chars: Array[Char], acc: Int): Boolean = {
+      if (chars.isEmpty) acc == 0
+      else {
+        val (first, rest) = (chars.head, chars.tail)
+        if (first == '(') cum_balance(rest, acc-1)
+        else if (first == ')' && acc < 0) cum_balance(rest, acc+1)
+        else false
+      }
+    }
+    val parenthesis = chars.filter("()".contains(_))
+    cum_balance(parenthesis, acc=0)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, imb_open: Int, imb_close: Int): (Int, Int) = {
+      if (idx == until) (imb_open, imb_close)
+      else  chars(idx) match {
+        case '(' => traverse(idx+1, until, imb_open+1, imb_close)
+        case ')' =>
+          if (imb_open > 0) traverse(idx+1, until, imb_open-1, imb_close)
+          else traverse(idx+1, until, imb_open, imb_close+1)
+        case _ => traverse(idx+1, until, imb_open, imb_close)
+      }
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from < threshold) traverse(from, until, 0, 0)
+      else {
+        val m = (from + until) / 2
+        val ((imb_open1, imb_close1), (imb_open2, imb_close2)) = parallel(
+          traverse(from, m, 0, 0), traverse(m, until, 0, 0)
+        )
+        (imb_open1 - imb_open2, imb_close1 - imb_close2)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
